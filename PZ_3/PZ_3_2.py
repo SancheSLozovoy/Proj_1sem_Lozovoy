1e336f564078db83a8bd26731b5fbf190c363ef9
SancheSLozovoy <alex.lozovoy05@bk.ru>
2022-10-22 16:04:03 +0300

    commit #1

--- PZ_3/PZ_3_2.py
+++ PZ_3/PZ_3_2.py
@@ -2,12 +2,12 @@ try:
     A = int(input("Введите целое число: "))
     B = int(input("Введите второе целое число: "))
     if A != B:
-        if A>B:
-            B=A
+        if A > B:
+            B = A
         else:
-            A=B
+            A = B
     else:
-        A=B=0
-    print(A,B)
+        A = B = 0
+    print(A, B)
 except:
     print("Ошибка, введены некоректные данные!")
