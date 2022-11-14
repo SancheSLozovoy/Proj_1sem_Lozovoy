eb299781a8b059771b8199236478db0d1f6d7abf
SancheSLozovoy <alex.lozovoy05@bk.ru>
2022-11-14 14:10:24 +0300

    commit

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -1,7 +1,8 @@
 # Дан список A размера N. Вывести вначале его элементы с нечетными номерами в
 # порядке возрастания номеров, а затем — элементы с четными номерами в порядке
 # убывания номеров: A1, A3, А5, …, A6, A4, A2. Условный оператор не использовать.
-n = int(input('введите кол-во символов: '))
+import random
+n = random.randint(2, 20)
 i = 0
 while i < n:
     a = list(range(1, n + 1))
@@ -10,4 +11,4 @@ for element in a:
     lstM = list(a[0::2])
     lstMch = list(a[1::2])
     lstMch.sort(reverse=True)
-print(*lstM, *lstMch)
+print(*lstM, *lstMch, sep=", ")
--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -1,7 +1,13 @@
 # Дан список размера N. Осуществить циклический сдвиг элементов списка вправо на
 # одну позицию (при этом A1 перейдет в A2, A2 — в A3,..., AN — в A1).
-n = int(input("Введите размер списка: "))
-a = [i for i in range(1, n + 1)]
-a.pop()
-a.insert(0, n)
-print(*a)
+import random
+n = random.randint(1, 20)
+a = []
+n1 = 0
+while n1 != n:
+    a.append(random.randint(-20, 20))
+    n1 += 1
+print("Первоначальный список:", *a)
+a.insert(0, a[-1])
+del a[-1]
+print("Результат", *a, sep=", ")
