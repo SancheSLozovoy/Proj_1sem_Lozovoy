b06bfc523c09d84a46ef9aa823b42d68a8af7872
SancheSLozovoy <alex.lozovoy05@bk.ru>
2022-12-03 15:42:51 +0300

    commit

--- PZ_9/PZ_9_1.py
+++ PZ_9/PZ_9_1.py
@@ -1,5 +1,5 @@
 # Выполнить сортировку словаря d = {'a': 1, 'b': 2, 'c': 3}
 d = {'a': 1, 'b': 2, 'c': 3}
 print("Результат сортировки:")
-for i in sorted(d):
-    print(i, d[i])
+for i in sorted(d.items(), key=lambda sort: (sort[0], sort[1])):
+    print(*i)
