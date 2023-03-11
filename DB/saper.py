c95b16c5ea44a21ce8c7a259a33da7d1b621da86
banany14141 <vitalikermolovich@gmail.com>
2023-03-11 19:13:56 +0300

    commit

---
+++ DB/saper.py
@@ -0,0 +1,44 @@
+from numpy import printoptions
+import sqlite3 as sq
+from data_users import info_users
+
+# with sq.connect('saper.db') as con:
+  #  con.execute("""CREATE TABLE IF NOT EXISTS users (
+   #     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #    name TEXT NOT NULL,
+     #   sex INTEGER NOT NULL DEFAULT 1,
+      #  old INTEGER,
+       # score INTEGER
+    #)""")
+    #con.executemany("INSERT INTO users VALUES (?,?,?,?,?)", info_users)
+    #con.commit()
+
+with sq.connect("saper.db") as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE score > 1100 and old <=14")
+    res = cur.fetchall()
+print(res)
+
+with sq.connect("saper.db") as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE old IN(12, 32) AND score > 1000")
+    res = cur.fetchall()
+print(res)
+
+with sq.connect("saper.db") as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE old IN(12, 32) AND score > 1000 OR sex ==1")
+    res = cur.fetchall()
+print(res)
+
+with sq.connect("saper.db") as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE (old IN(12, 32) OR sex = 2) AND score > 200")
+    res = cur.fetchall()
+print(res)
+
+with sq.connect("saper.db") as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE score < 1100 ORDER BY old")
+    res = cur.fetchall()
+print(res)
