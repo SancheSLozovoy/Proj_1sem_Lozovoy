66f9b618ff6df040104c6073acf7b5ce4413888f
SancheSLozovoy <alex.lozovoy05@bk.ru>
2023-03-24 22:54:38 +0300

    commit

--- DB/saper.py
+++ DB/saper.py
@@ -1,4 +1,3 @@
-from numpy import printoptions
 import sqlite3 as sq
 from data_users import info_users
 
@@ -13,32 +12,56 @@ from data_users import info_users
 #con.executemany("INSERT INTO users VALUES (?,?,?,?,?)", info_users)
 #con.commit()
 
-with sq.connect("saper.db") as con:
-    cur = con.cursor()
-    cur.execute("SELECT * FROM users WHERE score > 1100 and old <=14")
-    res = cur.fetchall()
-print(res)
-
-with sq.connect("saper.db") as con:
-    cur = con.cursor()
-    cur.execute("SELECT * FROM users WHERE old IN(12, 32) AND score > 1000")
-    res = cur.fetchall()
-print(res)
-
-with sq.connect("saper.db") as con:
-    cur = con.cursor()
-    cur.execute("SELECT * FROM users WHERE old IN(12, 32) AND score > 1000 OR sex ==1")
-    res = cur.fetchall()
-print(res)
-
-with sq.connect("saper.db") as con:
-    cur = con.cursor()
-    cur.execute("SELECT * FROM users WHERE (old IN(12, 32) OR sex = 2) AND score > 200")
-    res = cur.fetchall()
-print(res)
-
-with sq.connect("saper.db") as con:
-    cur = con.cursor()
-    cur.execute("SELECT * FROM users WHERE score < 1100 ORDER BY old")
-    res = cur.fetchall()
-print(res)
+# with sq.connect("saper.db") as con:
+#     cur = con.cursor()
+#     cur.execute("SELECT * FROM users WHERE score > 1100 and old <=14")
+#     res = cur.fetchall()
+# print(res)
+#
+# with sq.connect("saper.db") as con:
+#     cur = con.cursor()
+#     cur.execute("SELECT * FROM users WHERE old IN(12, 32) AND score > 1000")
+#     res = cur.fetchall()
+# print(res)
+#
+# with sq.connect("saper.db") as con:
+#     cur = con.cursor()
+#     cur.execute("SELECT * FROM users WHERE old IN(12, 32) AND score > 1000 OR sex ==1")
+#     res = cur.fetchall()
+# print(res)
+#
+# with sq.connect("saper.db") as con:
+#     cur = con.cursor()
+#     cur.execute("SELECT * FROM users WHERE (old IN(12, 32) OR sex = 2) AND score > 200")
+#     res = cur.fetchall()
+# print(res)
+#
+# with sq.connect("saper.db") as con:
+#     cur = con.cursor()
+#     cur.execute("SELECT * FROM users WHERE score < 1100 ORDER BY old")
+#     res = cur.fetchall()
+# print(res)
+#
+# with sq.connect("saper.db") as con:
+#     cur = con.cursor()
+#     cur.execute("UPDATE users SET old = 18 WHERE old = 20")
+#     res = cur.fetchall()
+# print(res)
+#
+# with sq.connect("saper.db") as con:
+#     cur = con.cursor()
+#     cur.execute("UPDATE users SET score = score+300 WHERE old <= 12 ")
+#     res = cur.fetchall()
+# print(res)
+#
+# with sq.connect("saper.db") as con:
+#     cur = con.cursor()
+#     cur.execute("UPDATE users SET score = score+100 WHERE old = 15 ")
+#     res = cur.fetchall()
+# print(res)
+#
+# with sq.connect("saper.db") as con:
+#     cur = con.cursor()
+#     cur.execute("DELETE FROM users WHERE score < 1000 ")
+#     res = cur.fetchall()
+# print(res)
